178081b8b3213d61b93e908ef074fb608a2d40e9
Ahmed <ah1973826450@gmail.com>
2025-11-05 11:24:31 +0200

    update CORS

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:3000",
-            "https://facebook-cloning-next.vercel.app/",
+            "https://facebook-cloning-next.vercel.app",
             "https://facebook-cloning-backend.vercel.app"
         ],
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
